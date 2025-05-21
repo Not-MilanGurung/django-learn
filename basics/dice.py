c27802b8c28831c056cad76eb777d0e8706d6bc1
Not-MilanGurung <notmilangurung@gmail.com>
2025-05-21 09:07:23 +0545

    Just added simple tasks given in class

---
+++ basics/dice.py
@@ -0,0 +1,19 @@
+import random
+
+def roll():
+	print("\nEnter 1 to roll the dice")
+	x = int(input("Enter your choice: "))
+	if (x == 1):
+		num = random.randint(1,6)
+		di = int(input("\nPredict the dice roll (1 to 6):"))
+		if (di == num):
+			print("You successfully predicted the roll\n")
+		else:
+			print("Sorry the dice rolled "+str(num))
+		roll()
+	else:
+		print("Exiting")
+
+
+if __name__ == "__main__":
+	roll()
