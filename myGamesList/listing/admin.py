d6079541c27d11460af339494f18495b8e2ed0de
Not-MilanGurung <notmilangurung@gmail.com>
2025-08-14 13:00:46 +0545

    New models for listing

--- myGamesList/listing/admin.py
+++ myGamesList/listing/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
-from .models import Genre, Game, Play
+from .models import Genre, Platform, StoreFront, PlayType, Game
 
 class GenresAdmin(admin.ModelAdmin):
-	list_display = ('name',)
+	list_display = ("name",)
+
+class PlatformAdmin(admin.ModelAdmin):
+	list_display = ("name",)
+
+class StoreFrontAdmin(admin.ModelAdmin):
+	list_display = ("name",)
+
+class PlayTypeAdmin(admin.ModelAdmin):
+	list_display = ("name",)
 
 class GamesAdmin(admin.ModelAdmin):
-	list_display = ('name', 'noOfReplays', 'rating')
+	list_display = ("name", "release_date")
 
 
 admin.site.register(Genre, GenresAdmin)
+admin.site.register(Platform, PlatformAdmin)
+admin.site.register(StoreFront, StoreFrontAdmin)
+admin.site.register(PlayType, PlayTypeAdmin)
 admin.site.register(Game, GamesAdmin)
--- myGamesList/listing/models.py
+++ myGamesList/listing/models.py
@@ -1,27 +1,57 @@
 from django.db import models
 
 class Genre(models.Model):
-	name = models.CharField(max_length=255, primary_key=True)
+	name = models.CharField(max_length=128, unique=True)
+	description = models.TextField(blank=True)
 
 	def __str__(self):
 		return self.name
 
+class Platform(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	description = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.name
+	
+class StoreFront(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	description = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.name
+
+
 class Game(models.Model):
-	name = models.CharField(max_length=255, primary_key=True)
-	noOfReplays = models.IntegerField(default=0)
-	rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)
-	genre = models.ForeignKey(Genre, null=False, on_delete=models.PROTECT)
-	owner = models.ForeignKey('auth.User', related_name='games', on_delete=models.CASCADE)
+	name = models.CharField(max_length=255)
+	description = models.TextField(blank=True)
+	release_date = models.DateField(blank=True, null=True)
+	developer = models.CharField(max_length= 128, blank=True)
+	publisher = models.CharField(max_length= 128, blank=True)
+	genres = models.ManyToManyField(Genre, related_name="games", blank=True)
+	platforms = models.ManyToManyField(Platform, related_name="games", blank=True)
+	owner = models.ForeignKey("auth.User", related_name="games", on_delete=models.CASCADE, null=True, blank=True)
+	cover_image = models.ImageField(upload_to="covers/", blank=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+class PlayType(models.Model):
+	name = models.CharField(max_length=32, unique=True)
 
 	def __str__(self):
 		return self.name
 
 class Play(models.Model):
 	game = models.ForeignKey(Game, on_delete=models.CASCADE)
-	replay = models.BooleanField(default=False)
-	timeSpentPlaying = models.DurationField(null=True)
-	startDate = models.DateField(null=True)
-	completedDate = models.DateField(null=True)
+	platform = models.ForeignKey(Platform, on_delete=models.SET_NULL, null=True)
+	storefront = models.ForeignKey(StoreFront, on_delete=models.SET_NULL, null=True)
+	play_type = models.ManyToManyField(PlayType)
+	time_spent_playing = models.DurationField(null=True)
+	note = models.TextField(null=True)
+	start_date = models.DateField(null=True)
+	completion_date = models.DateField(null=True)
+	rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)
 
 	def __str__(self):
 		return f'{self.game} {self.startDate}'
