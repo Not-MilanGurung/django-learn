a5e8610cc3bf689188d17d39ba38fa596bc9d8b5
Not-MilanGurung <notmilangurung@gmail.com>
2025-09-29 09:14:25 +0545

    Removed test project

--- test/simapple/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', views.homepage, name="HomePage"),
-	path('people/', views.people, name="People"),
-	path('people/details/<int:id>', views.details, name="Details")
-]
