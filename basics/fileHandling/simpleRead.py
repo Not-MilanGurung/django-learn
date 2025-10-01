81449281e3c3fba2cb7fa6d8863fea14f3d195e4
Not-MilanGurung <notmilangurung@gmail.com>
2025-10-01 16:40:42 +0545

    Removed the basics/ dir as it only contains basic python lessons

--- basics/fileHandling/simpleRead.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-	f = open("fileHandling/text.txt", "rt")
-	print(f.readline())
-	a = [1,2,3]
-	b = [1,2,3]
-	a.extend(b)
-	print(a)
--- basics/raise.py
+++
@@ -1,4 +0,0 @@
-a = 50
-num = int(input("Enter a number to divide "+str(a)+" : "))
-if (num == 0):
-	raise Exception("The value can not be zero")
